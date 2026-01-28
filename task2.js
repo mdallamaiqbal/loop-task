d4d781cb280f2776ae292decce175cf7e3c8a1ec
mdallamaiqbal <bd999plus13@gmail.com>
2026-01-28 10:45:39 +0600

    odd/even task using while loop

---
+++ task2.js
@@ -0,0 +1,28 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+let nums =61;
+while(nums<100){
+    if(nums % 2 !==0){
+        console.log('Number is odd:', nums);
+    }
+    nums++;
+}
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+let numbers =78;
+while(numbers<=98){
+    if(numbers % 2 ===0){
+        console.log('Number is even:', numbers);
+    }
+    numbers++
+}
