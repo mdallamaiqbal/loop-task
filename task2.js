3bcfa5dc16515f2838cf261ccc222862cb09d264
mdallamaiqbal <bd999plus13@gmail.com>
2026-01-28 10:55:04 +0600

    odd/even task2 using while loop

--- task2.js
+++ task2.js
@@ -3,6 +3,10 @@
 Subtask-1:
 
 Find all the odd numbers from 61 to 100.
+Subtask-1.2:
+
+Display sum of all the odd numbers from 81 to 131.
+
 
  */
 let nums =61;
@@ -12,11 +16,22 @@ while(nums<100){
     }
     nums++;
 }
+let sum = 81;
+while(sum<=131){
+    if(sum % 2 ===1){
+        console.log(sum);
+    }
+    sum++
+}
 /***
 
-Subtask-2:
+Subtask-2.1:
 
 Find all the even numbers from 78 to 98.
+
+Subtask-2.2:
+
+Display sum of all the even numbers from 206 to 311.
 
  */
 let numbers =78;
@@ -25,4 +40,12 @@ while(numbers<=98){
         console.log('Number is even:', numbers);
     }
     numbers++
+}
+
+let sumNum = 206;
+while(sumNum<311){
+    if(sumNum % 2 ===0){
+        console.log(sumNum);
+    }
+    sumNum++;
 }
