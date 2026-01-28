7415d780b6544f995057fe45c61dd613ead3f2ab
mdallamaiqbal <bd999plus13@gmail.com>
2026-01-28 11:36:18 +0600

    counts down using while loop

---
+++ task4.js
@@ -0,0 +1,10 @@
+/*===========While Loop Task=============== */
+/***
+Implement a countdown timer that counts down from 21 to 15.
+*/
+let currentNumber =21;
+while(currentNumber>=15){
+    console.log(currentNumber);
+    currentNumber--;
+}
+
