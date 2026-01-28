334f4265ca4cb7334190c1f51b1b3748887413c4
mdallamaiqbal <bd999plus13@gmail.com>
2026-01-28 11:13:23 +0600

    multiplication using while loop

---
+++ task3.js
@@ -0,0 +1,13 @@
+/*===========While Loop Task=============== */
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+let counter =1;
+let number =5;
+while(counter<=10){
+    console.log(number+' X '+counter+' = '+(number * counter));
+    
+    counter++
+}
